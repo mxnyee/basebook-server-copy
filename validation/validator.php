33c8b4bbb7bcac0e4cdfb6820107fe850eae63dc
Maxine Yee <maxineyeee@gmail.com>
2020-04-04 12:33:58 -0700

    Make validation a service

--- validation/validator.php
+++ validation/validator.php
@@ -1,71 +1,80 @@
 <?php
 use JsonSchema\SchemaStorage;
-use JsonSchema\Validator;
+use JsonSchema\Validator as JsonValidator;
 use JsonSchema\Constraints\Factory;
 use JsonSchema\Constraints\Constraint;
 
 require_once 'schemaBuilder.php';
 
-function createValidator() {
-  $schemaStorage = new SchemaStorage();
-  return $schemaStorage;
-}
+class Validator {
+  private $schemaStorage;
+  private $paramValidator;
 
-function validate($schemaStorage, $params, $body, $validParams, $validFields, $requiredFields, $useDependencies) {
-  validateParams($params, $validParams);
-  validateBody($schemaStorage, $body, $validFields, $requiredFields, $useDependencies);
-  return fillMissingFields($body, $validFields);
-}
+  public function __construct() {
+    $this->schemaStorage = new SchemaStorage();
+    $this->paramValidator = new JsonValidator();
+  }
+  
 
-function validateParams($data, $validData) {
-  // Can't have an empty enum, so manually check $data when $validData is empty
-  if (empty($validData)) {
-    if (!empty($data)) {
-      throw new BadRequestException('Extraneous parameters or data.');
-    } else {
-      return;
+  public function validate($params, $body, $validParams, $validFields, $requiredFields, $useDependencies) {
+    $this->validateParams($params, $validParams);
+    $this->validateBody($body, $validFields, $requiredFields, $useDependencies);
+    return $this->fillMissingFields($body, $validFields);
+  }
+
+
+  private function validateParams($data, $validData) {
+    // Can't have an empty enum, so manually check $data when $validData is empty
+    if (empty($validData)) {
+      if (!empty($data)) {
+        throw new BadRequestException('Extraneous parameters or data.');
+      } else {
+        return;
+      }
+    }
+
+    if (is_null($data)) {
+      throw new BadRequestException('Data is null.');
+    }
+    
+    $array = array_keys($data);
+    $enum = buildEnum($validData);
+
+    $this->paramValidator->validate($array, $enum);
+    
+    if (!$this->paramValidator->isValid()) {
+      $error = $this->paramValidator->getErrors()[0];
+      $err = ' [' . $array[$error['property'][1]] . '] ' . $error['message'];
+      throw new BadRequestException($err);
     }
   }
 
-  if (is_null($data)) {
-    throw new BadRequestException('Data is null.');
-  }
-  
-  $array = array_keys($data);
-  $enum = buildEnum($validData);
 
-  $validator = new Validator();
-  $validator->validate($array, $enum);
-  
-  if (!$validator->isValid()) {
-    $error = $validator->getErrors()[0];
-    $err = ' [' . $array[$error['property'][1]] . '] ' . $error['message'];
-    throw new BadRequestException($err);
-  }
-}
+  private function validateBody($data, $validData, $requiredData, $useDependencies) {
+    $object = (object)$data;
+    $schema = buildSchema($validData, $requiredData, $useDependencies);
 
-function validateBody($schemaStorage, $data, $validData, $requiredData, $useDependencies) {
-  $object = (object)$data;
-  $schema = buildSchema($validData, $requiredData, $useDependencies);
+    // The path to validation/definitions.json relative to index.php
+    $this->schemaStorage->addSchema('../validation/definitions.json', $schema);
+    $bodyValidator = new JsonValidator( new Factory($this->schemaStorage) );
 
-  // The path to validation/definitions.json relative to index.php
-  $schemaStorage->addSchema('../validation/definitions.json', $schema);
-  $validator = new Validator( new Factory($schemaStorage) );
-
-  $validator->coerce($object, $schema);
-  
-  if (!$validator->isValid()) {
-    $error = $validator->getErrors()[0];
-    $err = ' [' . $error['property'] . '] ' . $error['message'];
-    throw new BadRequestException($err);
-  }
-}
-
-function fillMissingFields($body, $validFields) {
-  foreach($validFields as $field) {
-    if (!array_key_exists($field, $body)) {
-      $body[$field] = NULL;
+    $bodyValidator->coerce($object, $schema);
+    
+    if (!$bodyValidator->isValid()) {
+      $error = $bodyValidator->getErrors()[0];
+      $err = ' [' . $error['property'] . '] ' . $error['message'];
+      throw new BadRequestException($err);
     }
   }
-  return $body;
+
+
+  private function fillMissingFields($body, $validFields) {
+    foreach($validFields as $field) {
+      if (!array_key_exists($field, $body)) {
+        $body[$field] = NULL;
+      }
+    }
+    return $body;
+  }
+
 }
