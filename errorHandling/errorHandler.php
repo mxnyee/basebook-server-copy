a8ef35394cbc49591b4057acc11f13066d28cf8d
Maxine Yee <maxineyeee@gmail.com>
2020-04-05 00:07:01 -0700

    Added exception handler for duplicate entries

--- errorHandling/errorHandler.php
+++ errorHandling/errorHandler.php
@@ -7,7 +7,8 @@ function handleThrown($response, $error) {
     case 'BadRequestException': return handleBadRequest($response, $error->getMessage());
     case 'ForbiddenException': return handleForbidden($response, $error->getMessage());
     case 'NotFoundException': return handleNotFound($response, $error->getMessage());
-    case 'InternalServerErrorException': return handleInternalServerError($response, $error->getMessage());
+    case 'mysqli_sql_exception': 
+      if ($error->getCode() == 1062) return handleInternalServerError($response, 'Duplicate entry.');
     default: throw $error;
   }
 }
